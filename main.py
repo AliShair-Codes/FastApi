6a4d04a7368712fbaa2eb3ce8e840a8bd935f6dc
Ali Shair <alishair104@outlook.com>
2026-03-29 17:23:29 +0530

    post method in fastapi

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from fastapi import FastAPI, Path, HTTPException, Query
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field, computed_field
+from typing import Annotated, Literal
 import json
 
 app = FastAPI()
@@ -8,40 +11,49 @@ def load_data():
         data = json.load(f)
         return data
 
-@app.get("/")
-def hello():
-    return {"message": "Hello world"}
+def save_data(data):
+    with open('patients.json', 'w') as f:
+        json.dump(data, f)
 
-@app.get('/about')
-def about():
-    return {'message': "This is about page"}
+class Patient(BaseModel):
+    id: Annotated[str, Field(..., description='ID of the patient', examples=['p001'])]  
+    name: Annotated[str, Field(..., description='Name of the patient')]
+    city: Annotated[str, Field(..., description='City of the patient is living')]
+    age: Annotated[int, Field(..., gt=0, lt=120, description='Age of the patient')]
+    gender: Annotated[Literal['male', 'female', 'others'], Field(..., description='Gender of the patient')]
+    height: Annotated[float, Field(..., gt=0, description="Height of the patient in meters")]
+    weight: Annotated[float, Field(..., gt=0, description='Weight of the patient in kgs')]
 
-@app.get("/view")
-def view():
+    # computed field
+    @computed_field
+    @property
+    def bmi(self) -> float:
+        bmi = round(self.weight/(self.height**2), 2)
+        return bmi
+
+    @computed_field
+    @property
+    def verdict(self) -> str:
+        if self.bmi < 18.5:
+            return 'underweight'
+        elif self.bmi  < 25:
+            return 'normal'
+        elif self.bmi  < 30:
+            return 'normal'
+        else: 
+            return 'obese'
+            
+
+
+@app.post('/create')
+def create_patient(patient: Patient):
     data = load_data()
-    return data
-
-@app.get("/patient/{patient_id}")
-def view_patient(patient_id: str = Path(..., description='ID of the patient in the DB', example = 'p001')):
-    data = load_data()
-    if(patient_id in data):
-        return data[patient_id]
-    # return {'error': 'patient not found'}
-    raise HTTPException(status_code=404, detail='Patient not found')
-
-@app.get('/sort')
-def sort_patients(sort_by: str = Query(..., description='Sort on the basis of height, weight or bmi'), order: str = Query('asc', description='sort in asc or desc order')):
-    valid_fields = ['height', 'weight', 'bmi']
-
-    if sort_by not in valid_fields:
-        raise HTTPException(status_code=400, detail=f'Invalid field select from {valid_fields}')
-
-    if order not in ['asc', 'desc']:
-        raise HTTPException(status_code=400, detail=f'Invalid order select b/w asc and desc')
+    if patient.id in data:
+        raise HTTPException(status_code=400, detail='Patient already exist')
     
-    data = load_data()
+    # patient is is pydantic obj 
+    data[patient.id] = patient.model_dump(exclude=['id']) # converting pydantic obj to python dict and adding in file
+
+    save_data(data)
     
-    sort_order = True if order=='desc' else False
-    # reverse false means asc reverse true means desc
-    sorted_data = sorted(data.values(), key=lambda x: x.get(sort_by, 0), reverse=sort_order)
-    return sorted_data
+    return JSONResponse(status_code=201, content={'message': 'Patient created successfully'})
